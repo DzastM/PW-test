3718422e9e0eec67330ab7bd38ec05d66cfb12f4
Justyna Mokrzycka <justyna.mok@gmail.com>
2026-04-28 12:44:15 +0200

    small fix

--- tests/product-filtering-and-search.spec.ts
+++ tests/product-filtering-and-search.spec.ts
@@ -1,6 +1,4 @@
 import { test, expect } from "@playwright/test";
-import { loginData } from "../test-data/login.data"
-import { LoginPage } from "../pages/login.page";
 import { ProductFilteringAndSearchPage } from "../pages/productFilteringAndSearch.page"
 
 test.describe("Product filtering & search", () => {
